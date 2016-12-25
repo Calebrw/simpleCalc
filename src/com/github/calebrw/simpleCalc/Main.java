f421cda7abb6befaedec4271bf32aa7285f39bcf
calebrw <calebrw@gmail.com>
2016-12-24 23:04:32 -0600

    Implement Factorial calculation

--- src/com/github/calebrw/simpleCalc/Main.java
+++ src/com/github/calebrw/simpleCalc/Main.java
@@ -16,9 +16,10 @@ public class Main {
             final String input1 = s.nextLine();
             System.out.print("Please enter a number: ");
             final String input2 = s.nextLine();
-            final String operators = "(+ - * /)";
-            System.out.print("Please input of of the following " + operators + ": ");
+            final String operators = "(+ - * / !)";
+            System.out.print("Please input one of the following " + operators + ": ");
             final String input3 = s.nextLine();
+            // For the operator, it is imperative that only the first character is used
             final char operator = input3.trim().charAt(0);
 
             // Convert Stings to Integers
@@ -40,6 +41,9 @@ public class Main {
                 case '/':
                     result = i1 / i2;
                     break;
+                case '!':
+                    result = factorial(i1);
+                    break;
             }
 
             if (result == null) {
@@ -59,6 +63,30 @@ public class Main {
 
             e.printStackTrace();
 
+        }
+
+    }
+
+    /**
+     * This static function returns the factorial value of an {@code Integer}
+     *
+     * @param d The initial integer as a {@code Double}, later rounded to nearest Integer
+     * @return The returned value as n * n-1 * ... * 1
+     */
+    static private Double factorial(Double d) {
+
+        int i = (int)Math.round(d);
+
+        if (i > 1) {
+            int result = i;
+            for (int j = i; j > 1; j--) {
+                result *= (j - 1);
+            }
+            return (double)result;
+        } else if (i == 1) {
+            return 1.0d;
+        } else {
+            throw new NumberFormatException("You cannot perform a factorial on a number less than 1");
         }
 
     }
