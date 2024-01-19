7928344aad4e7c080d9212c06fb249f31c41719a
Calebrw <calebrw@gmail.com>
2024-01-19 14:27:44 -0600

    Add code comments

--- src/com/github/calebrw/simpleCalc/MortCalc.java
+++ src/com/github/calebrw/simpleCalc/MortCalc.java
@@ -5,28 +5,37 @@ import java.util.Scanner;
 
 public class MortCalc {
 
+    // Define constants
     private static final int MONTHS_IN_YEAR = 12;
 
     public static void main(String[] args) {
 
-        double principal = 0;
-        float annualInterest = 0;
-        int termYears = 0;
+        // Declare variables
+        double principal;
+        float annualInterest;
+        int termYears;
+
         try {
             Scanner scanner = new Scanner(System.in);
 
+            // The principal is the total amount of the loan
             System.out.println("Enter the principal amount:");
             principal = scanner.nextDouble();
 
+            // The Annual Interest Rate
             System.out.println("Enter the annual interest rate:");
             annualInterest = scanner.nextFloat();
 
+            // If the interest rate is larger than .5 (aka 50%)
+            // then divide by 100 to get the actual percentage.
+            // For example .61 is more likely ot be .61% that 61%
             if (annualInterest > .5) {
                 System.out.println("Interest Rate too high.");
                 System.out.println("Dividing by 100.");
                 annualInterest /= 100;
             }
 
+            // Many mortgages are in 10, 15, 20, and 30 year terms
             System.out.println("Enter the term in years:");
             termYears = scanner.nextInt();
 
@@ -39,6 +48,14 @@ public class MortCalc {
 
     }
 
+    /**
+     * Calculates the mortgage's monthly payment
+     * @param principal         The principal is the total amount of the loan
+     * @param annualInterest    The Annual Interest Rate
+     * @param termYears         The number of years the mortgage is for
+     *
+     * @see     <a href="https://en.wikipedia.org/wiki/Mortgage_calculator">Mortgage calculator on Wikipedia</a>
+     */
     private static void Calculation(double principal, float annualInterest, int termYears) {
         float monthlyInterest = annualInterest / MONTHS_IN_YEAR;
         int termMonths = termYears * MONTHS_IN_YEAR;
